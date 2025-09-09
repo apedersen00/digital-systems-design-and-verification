e96ccb8919ffa359ae3878955b15f93ca04a1304
andreasp <dev@andreaspedersen.com>
2025-09-09 14:13:12 +0200

    add q5 gray encoder

---
+++ homework_1/hdl/q5/sim_main.cpp
@@ -0,0 +1,56 @@
+//-------------------------------------------------------------------------------------------------
+//
+//  File: sim_main.cpp
+//  Description: Stimulus for testbench
+//
+//  Author:
+//      - A. Pedersen
+//
+//-------------------------------------------------------------------------------------------------
+
+#include <math.h>
+#include <bitset>
+#include <memory>
+#include <verilated.h>
+#include "Vtop.h"
+
+template <size_t N>
+std::string to_binary(unsigned int value) {
+    return std::bitset<N>(value).to_string();
+}
+
+// Legacy function required only so linking works on Cygwin and MSVC++
+double sc_time_stamp() { return 0; }
+
+int main(int argc, char** argv) {
+    Verilated::mkdir("logs");
+    const std::unique_ptr<VerilatedContext> contextp{new VerilatedContext};
+    contextp->debug(0);
+    contextp->randReset(2);
+    contextp->traceEverOn(true);
+    contextp->commandArgs(argc, argv);
+
+    const std::unique_ptr<Vtop> top{new Vtop{contextp.get(), "TOP"}};
+
+    // initialize
+    top->binary = 0;
+
+    const int IN_WIDTH = 4;
+
+    for (int i = 0; i < pow(2, IN_WIDTH); i++) {
+        contextp->timeInc(10);
+        top->binary = i;
+        top->eval();
+
+        VL_PRINTF("[%03" PRId64 "] Binary=%s Gray=%s\n",
+            contextp->time(),
+            to_binary<4>(top->binary).c_str(),
+            to_binary<4>(top->gray).c_str()
+        );
+    }
+
+    top->final();
+    contextp->statsPrintSummary();
+    Verilated::threadContextp()->coveragep()->write("logs/coverage.dat");
+    return 0;
+}
---
+++ homework_1/hdl/q5/top.sv
@@ -0,0 +1,34 @@
+//-------------------------------------------------------------------------------------------------
+//
+//  File: top.sv
+//  Description: top module for testbench
+//
+//  Author:
+//      - A. Pedersen
+//
+//-------------------------------------------------------------------------------------------------
+
+module top
+  (
+    input   logic [3:0]   binary,
+    output  logic [3:0]   gray
+  );
+
+    // DUT instance
+    bin2gray dut (
+      .binary(binary),
+      .gray(gray)
+    );
+
+    // Stimulus
+    initial begin
+
+      if ($test$plusargs("trace") != 0) begin
+        $dumpfile("logs/vlt_dump.vcd");
+        $dumpvars();
+      end
+
+      $display("[%0t] Starting simulation...", $time);
+    end
+
+endmodule
