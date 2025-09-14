e65ae6e965624fbf0749a3c6309ba6f94d11c347
andreasp <dev@andreaspedersen.com>
2025-09-14 20:03:42 +0200

    add NX-to-X mux module

---
+++ milestone_1/hdl/src/mux.sv
@@ -0,0 +1,34 @@
+//-------------------------------------------------------------------------------------------------
+//
+//  File: mux.sv
+//  Description: Parameterized NX-to-X multiplexer module.
+//
+//  Author(s):
+//      - A. Pedersen
+//      - J. Sadiq
+//      - J. Yang
+//
+//-------------------------------------------------------------------------------------------------
+
+//  mux #(
+//    .Bitwidth(),
+//    .N()
+//   ) mux_0 (
+//    .d_i(),
+//    .sel_i(),
+//    .d_o()
+//  );
+
+module mux #(
+    parameter   BitWidth  = 8,
+    parameter   N         = 8,
+    localparam  SelWidth  = $clog2(N)
+  ) (
+    input  logic [BitWidth-1:0] d_i [N-1:0],
+    input  logic [SelWidth-1:0] sel_i,
+    output logic [BitWidth-1:0] d_o
+  );
+
+  assign d_o = d_i[sel];
+
+endmodule
