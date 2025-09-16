1dfb2ca9121cf60cf6962e43d2310811c300d0e2
andreasp <dev@andreaspedersen.com>
2025-09-16 13:46:32 +0200

    add top module for vivado

--- milestone_1/hdl/src/top.sv
+++ milestone_1/hdl/src/top.sv
@@ -42,3 +42,36 @@ module top
     end
 
 endmodule
+
+/* Top module for Urbana board
+module top
+  (
+    input   logic [15:0] SW,
+    output  logic [15:0] LED
+  );
+
+    logic [5:0] a;
+    logic [5:0] b;
+    logic [2:0] op;
+    logic [5:0] out;
+    logic [2:0] flags;
+    
+    assign a      = SW[5:0];
+    assign b      = SW[11:6];
+    assign op     = SW[14:12];
+    assign out    = LED[5:0];
+    assign flags  = LED[15:13];
+
+    // ALU instance
+    alu #(
+      .BW(6)
+    ) alu_0 (
+      .in_a(a),
+      .in_b(b),
+      .opcode(op),
+      .out(out),
+      .flags(flags)
+    );
+
+endmodule
+*/
